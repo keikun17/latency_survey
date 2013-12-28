dcf6649a87d3c15551688ff9ed6f3264d5c87c7e
Buddy Magsipoc <keikun17@gmail.com>
2013-12-28 19:11:26 +0800

    Read bower assets。

--- config.rb
+++ config.rb
@@ -63,8 +63,11 @@ configure :build do
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
+  set :relative_link, true
 end
+
+sprockets.append_path File.join "#{root}", 'source/components'
