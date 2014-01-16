7abd8492cb1e53a1ea46be00f6bd6ead6ae41b91
Buddy Magsipoc <keikun17@gmail.com>
2014-01-16 12:10:23 +0800

    Emberjs entrypoint。

---
+++ source/javascripts/app.js
@@ -0,0 +1,12 @@
+// $(function(){
+//   App = Ember.Application.create()
+// 
+//   App.ApplicationRoute = Ember.Route.extend({
+//     model: function() {
+//       return Ember.$.getJSON("/").then(function(data){
+//         return data.splice(0, 3)
+//       })
+//     }
+//   })
+// 
+// })
