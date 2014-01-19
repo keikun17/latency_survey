6c27f9a08f9d8735f29000bddb2e84083905aae0
Buddy Magsipoc <keikun17@gmail.com>
2014-01-19 17:33:28 +0800

    Inject data into cacher method。

--- modules/result.rb
+++ modules/result.rb
@@ -1,15 +1,16 @@
 class ::Result
 
-  def self.shout
-    'hey'
+  def self.cached_data(data)
+    @cached_data ||= data.to_h.except('page', 'result').collect do |k,v|
+      hash = {"#{k}" => v}
+      Result.new(hash)
+    end
   end
 
   def initialize(serialized_object = {})
     @area = serialized_object.keys.first
-
     @game_latencies = serialized_object[@area]
     @games = serialized_object[@area].keys
-
   end
 
   def area
