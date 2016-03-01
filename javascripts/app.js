374f74f6e258e192ab11cdf8245783b0910bf020
keikun17@gmail.com <keikun17@gmail.com>
2016-03-01 23:03:13 +0800

    Automated commit at 2016-03-01 15:03:12 UTC by middleman-deploy 1.0.0

---
+++ javascripts/app.js
@@ -0,0 +1,13 @@
+// $(function(){
+//   App = Ember.Application.create()
+// 
+//   App.ApplicationRoute = Ember.Route.extend({
+//     model: function() {
+//       return Ember.$.getJSON("/").then(function(data){
+//         return data.splice(0, 3)
+//       })
+//     }
+//   })
+// 
+// })
+;
